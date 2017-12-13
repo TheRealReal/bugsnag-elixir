9170d9fe15eaf70a25c65f51dc1ca7777f800332
David Cuddeback <david.cuddeback@gmail.com>
2017-12-13 12:28:21 -0700

    fix handling of {:bad_match, [...]}

--- lib/bugsnag/logger.ex
+++ lib/bugsnag/logger.ex
@@ -10,32 +10,14 @@ defmodule Bugsnag.Logger do
     {:ok, :ok, new_keys}
   end
 
-  def handle_event({_level, gl, _event}, state)
-  when node(gl) != node() do
+  def handle_event({_level, gl, _event}, state) when node(gl) != node() do
     {:ok, state}
   end
 
-  def handle_event({:error_report, _gl, {_pid, _type, [message | _]}}, state)
-  when is_list(message) do
-    try do
-      error_info = message[:error_info]
+  def handle_event({:error_report, _gl, {_pid, _type, [message | _]}}, state) when is_list(message) do
+    {_type, detail, stacktrace} = Keyword.fetch!(message, :error_info)
 
-      case error_info do
-        {_kind, {exception, stacktrace}, _stack} when is_list(stacktrace) ->
-          Bugsnag.report(exception, stacktrace: stacktrace)
-        {_kind, exception, stacktrace} ->
-          Bugsnag.report(exception, stacktrace: stacktrace)
-      end
-    rescue
-      ex ->
-        error_type = Exception.normalize(:error, ex).__struct__
-                      |> Atom.to_string
-                      |> String.replace(~r{\AElixir\.}, "")
-
-        reason = Exception.message(ex)
-
-        Logger.warn "Unable to notify Bugsnag #{error_type}: #{reason}"
-    end
+    Bugsnag.report(Exception.normalize(:error, detail, stacktrace), stacktrace: stacktrace)
 
     {:ok, state}
   end
